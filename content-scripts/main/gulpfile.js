a1fd1d796e79c2380885d8c1882c65dadfa35034
jmchugh <joshua.mchugh01@gmail.com>
2020-01-17 22:17:54 -0500

    Adjusted the content scripts module to have children for multiple content scripts apps

--- content-scripts/main/gulpfile.js
+++ content-scripts/main/gulpfile.js
@@ -4,12 +4,12 @@ var argv = require("yargs").argv;
 var webpack = require("webpack");
 var webpackStream = require("webpack-stream");
 
-gulp.task("clean", del.bind(null, ["../dist/content-scripts/**"], {force: true}));
+gulp.task("clean", del.bind(null, ["../../dist/content-scripts/**"], {force: true}));
 
 gulp.task("webpack", function() {
     return gulp.src("./src/main.js")
       .pipe(webpackStream(require("./webpack.config"), webpack))
-      .pipe(gulp.dest("../dist/content-scripts"));
+      .pipe(gulp.dest("../../dist/content-scripts/main"));
 });
 
 gulp.task("build", gulp.series(
